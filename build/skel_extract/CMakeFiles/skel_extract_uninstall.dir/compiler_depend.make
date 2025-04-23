3f2f0ad563d659add48b54ecd4155bb6092287e1
BjarkeHJ <bjarke_holm@hotmail.com>
2025-04-23 23:24:57 +0200

    remove build etc

--- build/skel_extract/CMakeFiles/skel_extract_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for skel_extract_uninstall.
-# This may be replaced when dependencies are built.
--- build/skel_extract/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bhj/OSEP/src/skel_extract
-# Build directory: /home/bhj/OSEP/build/skel_extract
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
